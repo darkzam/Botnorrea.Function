fcbea00e37b6f14a85885d56817d21e65a349e9e
Zamir Narvaez <zamiralexisnarvaez@gmail.com>
2023-02-19 14:00:42 -0500

    Added NotificationFunction

---
+++ src/NotificationFunction.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Botnorrea.Functions
+{
+    public static class NotificationFunction
+    {
+        private static HttpClient Client = new HttpClient();
+
+        [FunctionName("NotificationFunction")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log,
+            ExecutionContext context)
+        {
+            var config = new ConfigurationBuilder()
+                                                 .SetBasePath(context.FunctionAppDirectory)
+                                                 .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                                                 .AddEnvironmentVariables()
+                                                 .Build();
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var json = JsonConvert.SerializeObject(new { message = requestBody });
+            var content = new StringContent(json);
+
+            await Client.PostAsync(config["Botnorrea.Webhook"], content);
+
+            return new OkResult();
+        }
+    }
+}
