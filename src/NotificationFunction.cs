4f27a9d1918cf8b67fb94bd286f0de4e3021f38b
Zamir Narvaez <zamiralexisnarvaez@gmail.com>
2023-02-19 14:42:36 -0500

    changed method

--- src/NotificationFunction.cs
+++ src/NotificationFunction.cs
@@ -17,24 +17,31 @@ namespace Botnorrea.Functions
         private static HttpClient Client = new HttpClient();
 
         [FunctionName("NotificationFunction")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
             ILogger log,
             ExecutionContext context)
         {
-            var config = new ConfigurationBuilder()
+            try
+            {
+                var config = new ConfigurationBuilder()
                                                  .SetBasePath(context.FunctionAppDirectory)
                                                  .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
                                                  .AddEnvironmentVariables()
                                                  .Build();
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-            var json = JsonConvert.SerializeObject(new { message = requestBody });
-            var content = new StringContent(json);
+                var json = JsonConvert.SerializeObject(new { message = requestBody });
+                var content = new StringContent(json);
 
-            await Client.PostAsync(config["Botnorrea.Webhook"], content);
+                await Client.PostAsync(config["Botnorrea.Webhook"], content);
 
-            return new OkResult();
+                return new OkResult();
+            }
+            catch(Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
         }
     }
 }
