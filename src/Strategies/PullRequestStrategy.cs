23e36b1138d59c555e030dc295e5b41af1e266dc
Zamir Narvaez <zamiralexisnarvaez@gmail.com>
2023-02-22 20:23:52 -0500

    Fixed error

--- src/Strategies/PullRequestStrategy.cs
+++ src/Strategies/PullRequestStrategy.cs
@@ -6,11 +6,11 @@ namespace Botnorrea.Functions.Strategies
     {
         public override string GetMessage(dynamic payload)
         {
-            var message = new PullRequestMessage(payload?.action,
-                                                 payload?.pull_request?.html_url,
-                                                 payload?.pull_request?.user?.login,
-                                                 payload?.pull_request?.title,
-                                                 payload?.pull_request?.merged);
+            var message = new PullRequestMessage(payload?.action.ToString(),
+                                                 payload?.pull_request?.html_url.ToString(),
+                                                 payload?.pull_request?.user?.login.ToString(),
+                                                 payload?.pull_request?.title.ToString(),
+                                                 payload?.pull_request?.merged.ToString());
 
             return message.Initialize();
         }
